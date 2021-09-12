3766f638ba4c58ca48ac6aa9cd6dbdbca1bfa522
ppigazzini <pasquale.pigazzini@gmail.com>
2021-09-13 00:07:40 +0200

    hack(server): set task duration to 180 seconds

--- server/fishtest/rundb.py
+++ server/fishtest/rundb.py
@@ -86,7 +86,7 @@ class RunDb:
         self.official_master_sha = self.kvstore.get("official_master_sha", 40 * "f")
         self.worker_runs = self.kvstore.get("worker_runs", {})
 
-        self.task_duration = 1800  # 30 minutes
+        self.task_duration = 180  # 3 minutes
         self.ltc_lower_bound = 40  # Beware: this is used as a filter in an index!
         self.pt_info = {
             "pt_version": "SF_17",
