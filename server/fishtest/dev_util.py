9a4efba76e8936e57eea0e03f794b720e884e766
ppigazzini <pasquale.pigazzini@gmail.com>
2023-04-13 22:27:16 +0200

    add dev_util.py

---
+++ server/fishtest/dev_util.py
@@ -0,0 +1,21 @@
+import cProfile
+import io
+import pstats
+from functools import wraps
+
+
+def profileit(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        result = func(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = pstats.SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return result
+
+    return wrapper
